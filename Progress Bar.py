62521c0f25c562941e7a2d590d4f80e9a6ffd936
Rohan-Python <80620860+Rohan-Python@users.noreply.github.com>
2025-05-31 22:10:43 +0530

    Update Progress Bar.py

--- Progress Bar.py
+++ Progress Bar.py
@@ -220,7 +220,7 @@ def main():
             soil_img = get_image_base64("PulloutboxDiagram.jpg")
             if soil_img:
                 st.markdown(
-                    f'<img src="data:image/jpg;base64,{soil_img}" style="width: 2000px;">',
+                    f'<img src="data:image/jpg;base64,{soil_img}" style="width: 3500px;">',
                     unsafe_allow_html=True
                 )
             st.markdown("<p style='text-align: center; font-weight: bold;'>Soil-Geogrid Interaction</p>",
