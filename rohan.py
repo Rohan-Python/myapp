d7daf770942021fa53fd8735846b817b18a5e44b
Rohan-Python <80620860+Rohan-Python@users.noreply.github.com>
2025-06-08 21:29:25 +0530

    Update rohan.py

--- rohan.py
+++ rohan.py
@@ -342,8 +342,8 @@ def main():
                 unit_weight = st.number_input("Unit Weight (kN/m³):", value=None, placeholder="Enter value", step=0.1,
                                               format="%.2f", key="geo_unit_weight")        
                 # Add this at the bottom of col1 - EXACT SAME TABS
-            	st.markdown("---")  # Optional divider
-            	tab1_bottom, tab2_bottom = st.tabs(["Soil Parameters", "Geogrid Parameters"])
+                st.markdown("---")  # Optional divider
+                tab1_bottom, tab2_bottom = st.tabs(["Soil Parameters", "Geogrid Parameters"])
 
             with col2:
                 soil_img = get_image_base64("PulloutboxDiagram.jpg")
