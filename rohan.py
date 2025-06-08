2e99670a6de1085a185f842fe881afd55f13a00a
Rohan-Python <80620860+Rohan-Python@users.noreply.github.com>
2025-06-08 20:20:26 +0530

    Update rohan.py

--- rohan.py
+++ rohan.py
@@ -335,12 +335,15 @@ def main():
                 phi = st.number_input("Φ' (degrees):", value=None, placeholder="Enter value", step=0.1, format="%.1f", key="geo_phi")
                 cohesion = st.number_input("Cohesion c' (kPa):", value=None, placeholder="Enter value", step=0.1,
                                            format="%.2f", key="geo_cohesion")
+		water_content = st.number_input("Water Content (%):", value=None, placeholder="Enter value", step=0.1,
+                                                format="%.1f", key="geo_water_content")    
+                soil_classification = st.selectbox("Soil Classification:", options=list(geogrid_classification_map.keys()), key="geo_soil_class")
+		d50 = st.number_input("D50 (mm):", value=None, placeholder="Enter value", step=0.1, format="%.2f", key="geo_d50")    
                 unit_weight = st.number_input("Unit Weight (kN/m³):", value=None, placeholder="Enter value", step=0.1,
                                               format="%.2f", key="geo_unit_weight")
-                water_content = st.number_input("Water Content (%):", value=None, placeholder="Enter value", step=0.1,
-                                                format="%.1f", key="geo_water_content")
-                d50 = st.number_input("D50 (mm):", value=None, placeholder="Enter value", step=0.1, format="%.2f", key="geo_d50")
-                soil_classification = st.selectbox("Soil Classification:", options=list(geogrid_classification_map.keys()), key="geo_soil_class")
+                
+                
+                
 
             with col2:
                 soil_img = get_image_base64("PulloutboxDiagram.jpg")
