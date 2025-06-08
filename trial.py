dc40a411759b0eea8c6860fd6d51550b381bb4e5
Rohan-Python <80620860+Rohan-Python@users.noreply.github.com>
2025-06-09 00:03:15 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -344,8 +344,7 @@ def main():
     
     if st.session_state.current_tab == "Geogrid":
         tab1, tab2 = st.tabs(["Soil Parameters", "Geogrid Parameters"])	
-
-            with col2:
+	    with col2:
                 soil_img = get_image_base64("PulloutboxDiagram.jpg")
                 if soil_img:
                     st.markdown(
